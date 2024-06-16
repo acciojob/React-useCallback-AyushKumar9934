2e7feab033db6bc3089451808bd997b296c655b0
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-16 10:52:52 +0530

    safd

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SkillList = ({ skills, onDelete }) => {
     return (
-      <ul>
+      <ul id='skill-list'>
         {skills.map((skill, idx) => (
           <li key={idx} id={`skill-number-${idx}`} onClick={() => onDelete(skill)}>
             {skill}
